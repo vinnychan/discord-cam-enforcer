7ecc3f518cfbbb89766bae654be1d7d34f52ea4d
Vincent Chan <v.chan36@gmail.com>
2021-01-17 22:55:30 -0800

    link channe id in success msg

--- commands/set-vc-require-cam.js
+++ commands/set-vc-require-cam.js
@@ -23,7 +23,7 @@ const execute = (message, args) => {
 
         // set channel
         // await db.setCamRequire(channelID, args[1]);
-        message.channel.send(`MOCK: set channel #${voiceChannel.name} to ${args[1]}`)
+        message.channel.send(`MOCK: set channel <#${voiceChannel.id}> to ${args[1]}`)
 }
 
 module.exports = {
